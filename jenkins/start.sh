052be36f400c0b6503e488d44d678fdf78d99170
Abel <abel0130@163.com>
2021-06-30 11:37:43 +0800

    jenkins.sh

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -5,3 +5,10 @@ systemctl stop jenkins
 systemctl restart jenkins
 systemctl enable jenkins
 systemctl disable jenkins
+
+
+卸载Jenkins
+rpm -e jenkins
+
+删除遗留文件:
+find / -iname jenkins | xargs -n 1000 rm -rf
