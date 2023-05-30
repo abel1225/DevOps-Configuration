744da14cddf5ef05f14fe26ab04c2b9154ec9c65
Abel <abel0130@163.com>
2023-05-30 10:42:06 +0800

    binlog备份脚本清理过期文件/目录

--- mysql/bin-back/binlog-back.sh
+++ mysql/bin-back/binlog-back.sh
@@ -3,7 +3,9 @@ dbhost="dbhost"
 username="username"
 password="password"
 port="port"
-backupDir="/root/binlog"
+backdate=$(date "+%Y%m%d")
+backupbaseDir="/data/binlog/bak/"
+backupDir="$backupbaseDir$backdate"
 binlogs=`mysql -h$dbhost -u$username -p$password -P$port -e 'show binary logs;' | awk '$1 ~ /mysql-bin.*/ {print $1}'`
 num=0
 for binlog in $binlogs;
@@ -41,3 +43,18 @@ for binlog in $binlogs;
       #fi
   #fi
   done
+#清理过期文件/目录
+ReservedNum=3  #保留文件数
+FileDir="$backupbaseDir"
+date=$(date "+%Y%m%d-%H%M%S")
+
+cd $FileDir   #进入备份目录
+FileNum=$(ls -l | grep '^d' | wc -l)   #当前有几个文件夹，即几个备份
+
+while(( $FileNum > $ReservedNum))
+do
+    OldFile=$(ls -rt | head -1)         #获取最旧的那个备份文件夹
+    echo  $date "Delete File:"$OldFile
+    rm -rf $FileDir/$OldFile
+    let "FileNum--"
+done
